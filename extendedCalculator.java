bddf648c084987d5d0ad8e4b72474bea15f6a2a6
Shashank <01fe22bca200@kletech.ac.in>
2024-02-20 10:34:45 +0530

    code for extended calculator

---
+++ extendedCalculator.java
@@ -0,0 +1,11 @@
+class ExtendedCalculator{
+    float square(float operand_1){
+        return (operand_1 * operand_1);
+    }
+
+    float cube(float operand_1){
+        return (operand_1 * operand_1 * operand_1);
+    }
+
+}
+
